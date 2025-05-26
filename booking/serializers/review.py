9ab33fd75afcbc2c4f9ab1ccf1b4b02b2fc568a8
ssani4 <ssani4@icloud.com>
2025-05-26 09:59:00 +0200

    Update model Review, ReviewListSerializer, ReviewCreateUpdateSerializer

--- booking/serializers/review.py
+++ booking/serializers/review.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
 
 from booking.models import User
 from booking.models.rent import Rental
@@ -8,11 +9,11 @@ from booking.models.review import Review
 class ReviewListSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         slug_field='username',
-        queryset=User.objects.filter(role="RENTER").all(),
+        read_only=True,
     )
     rental = serializers.SlugRelatedField(
         slug_field='title',
-        queryset=Rental.objects.all(),
+        read_only=True,
     )
 
     class Meta:
@@ -22,3 +23,45 @@ class ReviewListSerializer(serializers.ModelSerializer):
             'rating', 'comment', 'created_at',
         ]
         read_only_fields = fields
+
+
+class ReviewCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = [
+            'id', 'user', 'rental', 'booking',
+            'rating', 'comment', 'created_at',
+        ]
+
+    def validate_rating(self, value):
+        if not 1 <= value <= 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5")
+        return value
+
+    def validate_booking(self, value):
+        request = self.context.get('request')
+        if value and value.user != request.user:
+            raise serializers.ValidationError("You can only use your own bookings")
+        return value
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        rental = attrs.get('rental', getattr(self.instance, 'rental', None))
+        booking = attrs.get('booking', getattr(self.instance, 'booking', None))
+
+        if self.instance is None and request.user.role != 'RENTER':
+            raise PermissionDenied("Only users with the role 'RENTER' can create reviews")
+
+        if self.instance and self.instance.user != request.user:
+            raise PermissionDenied("You can only edit your reviews")
+
+        if booking and booking.rental != rental:
+            raise serializers.ValidationError(
+                {"booking": "Booking must be for the same rental as the review"}
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
